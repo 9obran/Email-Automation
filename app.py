a3f561baf126e55db04e624e4f7415f44454c921
Omar Gobran <62971186+9obran@users.noreply.github.com>
2024-11-14 00:17:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_root)
 
 from src.app import app
+server = app.server
 
 if __name__ == '__main__':
     app.run_server(debug=True)
